f68596e79d113d3d7357b968e3a277169edbf393
Alexander <sashafromlibertalia@gmail.com>
2022-01-24 23:07:00 +0300

    feat: implemented `findCommon()` method

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,39 @@
 interface IArraise<T> {
     /**
      * Returns modified array with unique elements
+     * @param array - array to be modified
      * @example [1, 1, 2, 3] => [1, 2, 3]
      */
     makeUnique(array: Array<T>): Array<T>;
 
     /**
      * Sorts array in ascending order: 
+     * @param array - array to be sorted
      * @example [1, 5, 3] => [1, 3, 5]
      */
     sortAscending(array: Array<T>): Array<T>;
 
     /**
      * Sorts array in descending order: 
+     * @param array - array to be sorted
      * @example [1, 5, 3] => [5, 3, 1]
      */
     sortDescending(array: Array<T>): Array<T>;
 
+    /**
+     * Compares two arrays
+     * @param array1 - first array to compare
+     * @param array2 - second array to compare
+     */
     areSame(array1: Array<T>, array2: Array<T>): Boolean;
+
+
+    /**
+     * Returns array with common elements of both arrays
+     * @param array1 - first array 
+     * @param array2 - second array
+     */
+    findCommon(array1: Array<T>, array2: Array<T>): Array<T>;
 }
 
 export default class Arraise implements IArraise<any> {
@@ -26,9 +42,11 @@ export default class Arraise implements IArraise<any> {
     makeUnique(array: any[]): any[] {
         return [...new Set<any>(array)]
     }
+
     sortAscending(array: any[]): any[] {
         return array.sort()
     }
+
     sortDescending(array: any[]): any[] {
         return array.sort().reverse()
     }
@@ -44,4 +62,13 @@ export default class Arraise implements IArraise<any> {
         return true
     }
 
+    findCommon(array1: any[], array2: any[]): any[] {
+        let data = []
+        for (let i = 0; i < array1.length; i++) {
+            if (array2.includes(array1[i])) 
+                data.push(array1[i])
+        }
+
+        return data
+    }
 }
---
+++ tests/array/findCommon.test.ts
@@ -0,0 +1,25 @@
+import Arraise from "../../src/index"
+const arraise = new Arraise()
+
+describe("test common method", () => {
+    test("find common elements in arrays", () => {
+        const array1 = [1, 2, 3, 4, 5, 6, 7, 8]
+        const array2 = [1, 2, 3, 4]
+
+        expect(arraise.findCommon(array1, array2)).toStrictEqual([1, 2, 3, 4])
+    })
+
+    test("find common elements in arrays with string", () => {
+        const array1 = [1, "2", 3, 2, 4, 5, 6, 7, 8]
+        const array2 = [1, "2", 3, 4]
+
+        expect(arraise.findCommon(array1, array2)).toStrictEqual([1, "2", 3, 4])
+    })
+
+    test("don't find common elements in arrays", () => {
+        const array1 = [1, "2", 3, 2, 4, 5, 6, 7, 8]
+        const array2 = [11, 20]
+
+        expect(arraise.findCommon(array1, array2)).toStrictEqual([])
+    })
+})
